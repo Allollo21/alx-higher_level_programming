3fa148a2473e9d7375dea7b92e953e796ce3919b
Allollo21 <Aliredaer7@gmail.com>
2024-01-02 15:46:57 -0800

    Mandatory Tasks

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Allollo21
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Allollo21
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
